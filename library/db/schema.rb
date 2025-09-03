393aa676ea83333b901b9412549d1a013006e8e9
JuanCabelloVargas <jicabello@miuandes.cl>
2025-09-03 10:04:14 -0400

    feat: create authors model

--- library/db/schema.rb
+++ library/db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_03_131052) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_03_135601) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title", null: false
