eb98a67c0dee811fc3bf043f7c8bc1022ebefe7c
JuanCabelloVargas <jicabello@miuandes.cl>
2025-09-03 09:22:41 -0400

    feat: Create books table with timestamps, title and date

--- library/db/migrate/20250903131052_create_books_table.rb
+++ library/db/migrate/20250903131052_create_books_table.rb
@@ -1,6 +1,8 @@
 class CreateBooksTable < ActiveRecord::Migration[8.0]
   def change
-    create_table :books_tables do |t|
+    create_table :books do |t| #: refers to a symbol, its a special string that have ruby
+      t.string :title, null: false
+      t.date :publication_date, null: false
       t.timestamps
     end
   end
