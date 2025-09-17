e3cd471508f3f28b6cd2b180be6fd0d6efbc1306
JuanCabelloVargas <jicabello@miuandes.cl>
2025-09-17 09:37:30 -0300

    feat: Add association between author and books

--- library/db/schema.rb
+++ library/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_03_140601) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_17_114808) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -27,5 +27,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_03_140601) do
     t.date "publication_date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
+
+  add_foreign_key "books", "authors"
 end
--- library/db/seeds.rb
+++ library/db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+#
+puts "Esta es la seed"
+
+james_clear = Author.create!(first_name: "James", last_name: "Clear", rut: "1111111-1") 
+robert_greene = Author.create!(first_name: "Robert", last_name: "Greene", rut: "2222222222-2") 
+brandon_sanderson = Author.create!(first_name: "Brandon", last_name: "Sanderson", rut: "333333-3") 
+
+Book.create!(title: "Habitos Atomicos", publication_date: Date.new(2018, 10, 10), author_id: james_clear.id)
+Book.create!(title: "Maestria", publication_date: Date.new(2008, 02, 06), author_id: robert_greene.id)
+Book.create!(title: "Mistborn", publication_date: Date.new(2012, 06, 10), author_id: brandon_sanderson.id)
+
+
